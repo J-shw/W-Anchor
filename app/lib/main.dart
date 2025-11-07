54bb61ab7c7d7d55eacb1278760e7140b7913c8d
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-07 22:20:51 +0000

    Uses new main_screen + provider

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:w_anchor/screens/anchor_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:w_anchor/providers/anchor_provider.dart';
+import 'package:w_anchor/screens/main_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AnchorProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: AnchorScreen(),
+    return MaterialApp(
+      title: 'W Anchor',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainScreen(),
     );
   }
 }
