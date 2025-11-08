42b9f714b68db489f2508f7f7afb4092ab5b2ddd
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-08 08:35:13 +0000

    Create main_navigation_bar.dart

---
+++ app/lib/widgets/main_navigation_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MainNavigationBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onDestinationSelected;
+
+  const MainNavigationBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onDestinationSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return NavigationBar(
+      selectedIndex: selectedIndex,
+      onDestinationSelected: onDestinationSelected,
+      indicatorColor: Theme.of(context).colorScheme.primaryContainer,
+      destinations: <Widget>[
+        NavigationDestination(
+          selectedIcon: Icon(Icons.anchor_rounded),
+          icon: Icon(Icons.anchor_sharp),
+          label: "Anchor",
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.timeline_rounded),
+          icon: Icon(Icons.timelapse_sharp),
+          label: "History",
+        ),
+        NavigationDestination(
+          selectedIcon: Icon(Icons.settings),
+          icon: Icon(Icons.settings_outlined),
+          label: "Settings",
+        ),
+      ],
+    );
+  }
+}
