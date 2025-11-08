2044f2aef92c261d79be7156e3d8824c55afe539
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-08 08:07:21 +0000

    Passes settings provider

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:w_anchor/providers/anchor_provider.dart';
 import 'package:w_anchor/screens/main_screen.dart';
+import 'package:w_anchor/providers/settings_provider.dart';
+import 'package:w_anchor/utils/constants.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => AnchorProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => SettingsProvider()),
+        ChangeNotifierProxyProvider<SettingsProvider, AnchorProvider>(
+          create: (context) => AnchorProvider(),
+          update: (context, settings, anchor) {
+            if (anchor == null) return AnchorProvider();
+            anchor.updateSettings(settings);
+            return anchor;
+          },
+        ),
+      ],
       child: const MyApp(),
     ),
   );
@@ -17,11 +29,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeMode = context.watch<SettingsProvider>().theme;
+
     return MaterialApp(
       title: 'W Anchor',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      
+      themeMode: switch (themeMode) {
+        AppTheme.light => ThemeMode.light,
+        AppTheme.dark => ThemeMode.dark,
+        AppTheme.system => ThemeMode.system,
+      },
+      
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
       home: const MainScreen(),
     );
   }
