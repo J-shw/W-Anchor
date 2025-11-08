b0d6dd29e83915be8c72fa61fd704e490fb5c8c4
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-08 08:12:06 +0000

    Uses app colorscheme + added isLarge option

--- app/lib/widgets/info_box.dart
+++ app/lib/widgets/info_box.dart
@@ -3,39 +3,52 @@ import 'package:flutter/material.dart';
 class InfoBox extends StatelessWidget {
   final String title;
   final String value;
+  final bool isLarge;
 
   const InfoBox({
-    Key? key,
+    super.key,
     required this.title,
     required this.value,
-  }) : super(key: key);
+    this.isLarge = false,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Container(
       padding: const EdgeInsets.all(12.0),
       decoration: BoxDecoration(
-        color: Colors.grey.shade200,
+        color: colorScheme.secondaryContainer,
         borderRadius: BorderRadius.circular(12.0),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment:
+            isLarge ? MainAxisAlignment.center : MainAxisAlignment.start,
         children: [
           Text(
             title,
-            style: const TextStyle(
+            style: TextStyle(
               fontSize: 14,
-              color: Colors.black54,
+              color: colorScheme.onSecondaryContainer.withAlpha(80),
             ),
           ),
           const SizedBox(height: 4),
           Text(
             value,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-            maxLines: 2,
+            style: isLarge
+                ? TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: colorScheme.onSecondaryContainer,
+                  )
+                : TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: colorScheme.onSecondaryContainer,
+                  ),
+            maxLines: isLarge ? 1 : 2,
             overflow: TextOverflow.ellipsis,
           ),
         ],
