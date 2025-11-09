9300b8c0e5e1e893a1809fa91dc66404ef6c3aa1
J-shw <91017208+J-shw@users.noreply.github.com>
2025-11-09 20:39:14 +0000

    Create permission_screen.dart

---
+++ app/lib/screens/permission_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:w_anchor/screens/main_screen.dart';
+
+
+Future<bool> requestCriticalPermissions() async {
+  PermissionStatus locationStatus = await Permission.location.request();
+  
+  if (locationStatus.isDenied || locationStatus.isPermanentlyDenied) {
+    return false;
+  }
+
+  await Permission.notification.request();
+
+  PermissionStatus backgroundStatus = await Permission.locationAlways.status;
+
+  if (backgroundStatus != PermissionStatus.granted) {
+    backgroundStatus = await Permission.locationAlways.request();
+  }
+  
+  return backgroundStatus.isGranted;
+}
+
+class PermissionScreen extends StatefulWidget {
+  const PermissionScreen({super.key});
+
+  @override
+  State<PermissionScreen> createState() => _PermissionScreenState();
+}
+
+class _PermissionScreenState extends State<PermissionScreen> {
+  bool _permissionsGranted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAndRequestPermissions();
+  }
+
+  void _checkAndRequestPermissions() async {
+    await Permission.notification.request();
+    bool granted = await requestCriticalPermissions();
+    
+    if (mounted) {
+      setState(() {
+        _permissionsGranted = granted;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_permissionsGranted) {
+      return const MainScreen(); 
+    } else {
+      return const Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.lock_person, size: 50, color: Colors.red),
+              SizedBox(height: 16),
+              Text(
+                'Required permissions not granted.',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Please grant location and notification access to start monitoring.',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+}
